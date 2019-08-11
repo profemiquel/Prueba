24d1583ee34fb8f8756ce9f5f3af51377f37ae99
profemiquel <44139957+profemiquel@users.noreply.github.com>
2019-08-11 19:02:46 +0200

    Add files via upload

--- js/geo.js
+++ js/geo.js
@@ -8,11 +8,11 @@
                var longitud = position.coords.longitude;
 
               var div = document.getElementById("acces");
-              div.innerHTML = "<form name = 'formu' method='post' align='center' action='http://fatimasueca.es/apps/geo/paso.php'><input style='visibility:hidden' type='text' name='latitud' value='"+latitud+"'><input style='visibility:hidden' type='text' name='longitud' value="+longitud+"><br><button id='b1' type='submit'><strong><p>REGISTRE D'HORES <br><br>COL·LEGI NTRA. SRA. DE FÀTIMA</p></strong> </button</form>";}
+              div.innerHTML = "<form name = 'formu' method='post' align='center' action='http://fatimasueca.es/apps/geo/paso.php'><input style='visibility:hidden' type='text' name='latitud' value='"+latitud+"'><input style='visibility:hidden' type='text' name='longitud' value="+longitud+"><br><input type='text' name='usuari' placeholder='Nom usuari' width=25%><br><br><input type='password' name='pass' placeholder='Contrasenya' width=25%><br><br><button type='submit' class='boton0'><a>I N I C I</a></button></form>";}
 
       function refrescarUbicacion() {
                navigator.geolocation.watchPosition(mostrarUbicacion);}
                                  
      function errorCallback(error) {
-      alert("Error! Ha fallat l'app,\no tal vegada no funciona en el servidor.\n Surt i torna a entrar.");
-    }
+      alert("Error! Ha fallat l'app,\n o tal vegada no funciona en el servidor.\n Surt i torna a entrar.");
+    }            
